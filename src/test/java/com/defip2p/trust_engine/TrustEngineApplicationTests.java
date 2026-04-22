966cc02a6f8384c6b5b5cc13f77391306a58c2e7
Joseph <135732587+Josefuufuu@users.noreply.github.com>
2026-04-22 17:21:06 -0500

    fix: point TrustEngineApplicationTests at correct application class

--- src/test/java/com/defip2p/trust_engine/TrustEngineApplicationTests.java
+++ src/test/java/com/defip2p/trust_engine/TrustEngineApplicationTests.java
@@ -1,9 +1,10 @@
 package com.defip2p.trust_engine;
 
+import com.example.trustengine.TrustEngineApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = TrustEngineApplication.class)
 class TrustEngineApplicationTests {
 
 	@Test
